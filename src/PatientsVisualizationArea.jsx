be2eb17dc5be46ac4ae1e1c8bfb8a05a24e4553d
max neum <appprogrammierungkurs1115@gmail.com>
2023-07-31 11:03:53 +0200

    decrement with rank

--- src/PatientsVisualizationArea.jsx
+++ src/PatientsVisualizationArea.jsx
@@ -16,26 +16,43 @@ export function PatientsVisualizationArea() {
     const [heatVisibility, setHeatVisibility] = useState(false);
     const [biggestEmotion, setBiggestEmotion] = useState("happy");
     const [extdata, setExtdata] = useState([
-        {x: 350, y: 190, value: 0}, // happy
-        {x: 90, y: 50, value: 0}, //anger
-        {x: 50, y: 80, value: 0}, //contempt
-        {x: 20, y: 120, value: 0}, //disgust
-        {x: 170, y: 15, value: 0}, //fear
-        {x: 200, y: 200, value: 0}, //neutral
-        {x: 20, y: 250, value: 0}, //sad
-        {x: 240, y: 40, value: 0} //surprised
+        {x: 350, y: 190, value: -1}, // happy
+        {x: 90, y: 50, value: -1}, //anger
+        {x: 50, y: 80, value: -1}, //contempt
+        {x: 20, y: 120, value: -1}, //disgust
+        {x: 170, y: 15, value: -1}, //fear
+        {x: 200, y: 200, value: -1}, //neutral
+        {x: 20, y: 250, value: -1}, //sad
+        {x: 240, y: 40, value: -1} //surprised
+    ])
+    const [emoRank, setEmoRank] = useState([
+        {name: 'happy', rank: 3},
+        {name: 'anger', rank: 3},
+        {name: 'contempt', rank: 3},
+        {name: 'disgust', rank: 3},
+        {name: 'fear', rank: 3},
+        {name: 'neutral', rank: 3},
+        {name: 'sad', rank: 3},
+        {name: 'surprised', rank: 3}
     ])
 
 
+    const [emoSeriesIndex, setEmoSeriesIndex] = useState(0)
+
+
     const layout = [
-        {i: "vid", x: 0, y: 0, w: 3, h: 1},
-        {i: "ctrl", x: 3, y: 0, w: 1, h: 1},
-        {i: "va", x: 4, y: 0, w: 4, h: 1, isResizable: false},
-        {i: "heat", x: 8, y: 0, w: 4, h: 1, isResizable: false}
+        {i: "vid", x: 0, y: 0, w: 6, h: 1},
+        {i: "ctrl", x: 6, y: 0, w: 1, h: 1},
+        {i: "va", x: 7, y: 0, w: 4, h: 1, isResizable: false},
+        {i: "heat", x: 11, y: 0, w: 4, h: 1, isResizable: false}
     ];
 
 
-    /*Anfang aus heatjs*/
+    /*Anfang aus heatjs: mittels getEmoIndex aus Emotion
+    * Index von extdata ermitteln,
+    * wird in incrementExtdataValue genutzt
+    * */
+
     const emoMap = new Map()
     emoMap.set('happy', 0)
     emoMap.set('anger', 1)
@@ -49,24 +66,90 @@ export function PatientsVisualizationArea() {
 
     const getEmoIndex = () => emoMap.get(biggestEmotion);
 
-    function incrementExtdataValue() {
 
-        let locExtdata = extdata;
-        locExtdata[getEmoIndex()].value += 1
+    function incrementExtdataValue() {
+        let locExtdata = extdata
+        let locEmoRank = emoRank
+        let biggestEmoIndex = getEmoIndex()
+        locEmoRank[biggestEmoIndex].rank = 3
+        setEmoRank(locEmoRank)
+        locExtdata[biggestEmoIndex].value += 1
         setExtdata(locExtdata)
     }
-    //Ende aus heatjs
+
+    //Ende aus heatjs **********************************
 
 
-    function randomEmotion() {
-        let emos = ["sad", "disgust", "contempt", "anger", "fear", "surprised", "happy"]
-        let z = Math.floor(Math.random() * 7)
+    //mock-data
+    const emoSeries = [
+        "sad", "sad", "sad", "sad",
+        "disgust", "contempt", "anger", "anger",
+        "anger", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+        "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral", "neutral",
+    ]
 
-        return emos[z]
+    function decrementExtdataValue() {
+        let speci = getEmoIndex()
+        let locEmoRank = emoRank
+        let locExtdata = extdata
+
+        for (let i = 0; i < extdata.length; i++) {
+            console.log("i...." + i)
+            if (i !== speci) {
+
+                switch (emoRank[i].rank) {
+                    case 3:
+                        locEmoRank[i].rank = locEmoRank[i].rank - 1
+                        setEmoRank(locEmoRank)
+                        break
+                    case 2:
+                        locEmoRank[i].rank = locEmoRank[i].rank - 1
+                        setEmoRank(locEmoRank)
+                        break
+                    case 1:
+                        if (locExtdata[i].value > -1) {
+                            locExtdata[i].value = locExtdata[i].value - 1
+                        }
+                        setExtdata(locExtdata)
+                        break
+                }
+            }
+        }
+        console.log(speci)
+        console.log(extdata)
+        console.log(emoRank)
+    }
+
+
+    /*    function randomEmotion() {
+            let emos = ["sad", "disgust", "contempt", "anger", "fear", "surprised", "happy"]
+            let z = Math.floor(Math.random() * 7)
+
+            return emos[z]
+        }*/
+
+    function mockEmotion() {
+        let outp
+        (emoSeriesIndex < emoSeries.length) ? outp = emoSeries[emoSeriesIndex] : outp = emoSeries[0]
+        let newMockIndex
+        (emoSeriesIndex < emoSeries.length) ? newMockIndex = emoSeriesIndex + 1 : newMockIndex = 0
+        setEmoSeriesIndex(newMockIndex)
+        return outp
     }
 
     function changeBiggestEmotion(be) {
         incrementExtdataValue()
+        decrementExtdataValue()
         setBiggestEmotion(be)
     }
 
@@ -80,6 +163,7 @@ export function PatientsVisualizationArea() {
         setHeatVisibility(newValue)
     }
 
+
     return (
         <GridLayout
             className="layout"
@@ -88,14 +172,14 @@ export function PatientsVisualizationArea() {
             rowHeight={400}
             width={1700}
         >
+
             <div key="vid">
-                <Webcam height={800} width={900}/>
-                {/*<VideoContainer/>*/}
+                <Webcam height={400} width={500}/>
             </div>
 
             <div key="ctrl">
                 <ControllsContainer
-                    cBE={() => changeBiggestEmotion(randomEmotion())}
+                    cBE={() => changeBiggestEmotion(mockEmotion())}
                     cVAV={() => changeVaVisibility()}
                     cHeatV={() => changeHeatVisibility()}
                 />
